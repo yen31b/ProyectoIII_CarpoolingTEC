00963adf8bcf423e1c429fa246895694291db8dd
yen31b <38542884+yen31b@users.noreply.github.com>
2018-11-26 06:56:23 -0600

    Linkedin Login

---
+++ LoginLinkedin/AndroidLinkedinLogin/app/src/main/java/com/example/yen/androidlinkedinlogin/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.yen.androidlinkedinlogin;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+
+import com.linkedin.platform.LISession;
+import com.linkedin.platform.LISessionManager;
+import com.linkedin.platform.errors.LIAuthError;
+import com.linkedin.platform.listeners.AuthListener;
+import com.linkedin.platform.utils.Scope;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view){
+        //login activity
+
+        LISessionManager.getInstance(getApplicationContext()).init(this, buildScope(), new AuthListener() {
+            @Override
+            public void onAuthSuccess() {
+
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                intent.putExtra("value", LISessionManager.getInstance(getApplicationContext()).getSession().getAccessToken().toString());
+
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onAuthError(LIAuthError error) {
+
+            }
+        }, true);
+
+    }
+
+    //
+
+    private static Scope buildScope (){
+        return Scope.build(Scope.R_BASICPROFILE, Scope.R_EMAILADDRESS);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        LISessionManager.getInstance(getApplicationContext()).onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    private void getPackageHash() {
+        try {
+
+            @SuppressLint("PackageManagerGetSignatures") PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.yen.androidlinkedinlogin",//give your package name here
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+
+                Log.d(TAG, "Hash  : " + Base64.encodeToString(md.digest(), Base64.NO_WRAP));//Key hash is printing in Log
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d(TAG, e.getMessage(), e);
+        } catch (NoSuchAlgorithmException e) {
+            Log.d(TAG, e.getMessage(), e);
+        }
+
+    }
+
+
+}
